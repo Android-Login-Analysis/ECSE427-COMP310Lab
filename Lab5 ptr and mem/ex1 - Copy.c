b9dcd402cca16dd70f7f99eb5346cb78198e5de0
persist2018 <1606441033@qq.com>
2024-09-27 14:37:16 -0400

    update lab5 code

--- Lab5 ptr and mem/ex1 - Copy.c
+++ Lab5 ptr and mem/ex1 - Copy.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    int a[4] = { 4, 3, 2, 1 };
+    int a[4] = {4, 3, 2, 1};
     int *p = a;
-    printf("p is %p\\n", p);
-    printf("p + 6 is %p\\n", p + 6);
+    printf("p is %p\\n", (void *)p);
+    printf("p + 6 is %p\\n", (void *)(p + 6));
     return 0;
 }
