44a185ddcca52a683b42753d4a599d822ea61720
persist2018 <1606441033@qq.com>
2024-10-01 08:44:10 -0400

    update lab6 c files

--- Lab6 C Files/myheader.c
+++ Lab6 C Files/myheader.c
@@ -1,7 +1,7 @@
 // main.c
 #include <stdio.h>
 #include "myheader.h"
-#include "myheader.h" // Even if this is included twice, #pragma once prevents multiple inclusion
+#include "myheader.h" // Even if this is included twice, #pragma once prevents multiple inclusion!
 
 void myFunction()
 {
